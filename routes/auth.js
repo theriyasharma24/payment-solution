4b114df96f101a3130cf47e24f26370a9c3eb72b
Sabhya <sabhya12.saini@gmail.com>
2022-01-09 00:08:30 +0530

    Resolve critical issue#1

--- routes/auth.js
+++ routes/auth.js
@@ -39,7 +39,7 @@ router.post(
     const { email, password } = req.body;
 
     try {
-      let user = await User.findOne([{ email }]);
+      let user = await User.findOne({ email });
 
       if (!user) {
         return res.status(400).json({ msg: "Invalid Credentials" });
