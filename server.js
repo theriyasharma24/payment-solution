7b3830c08e5d6e7a6837adda4168aea3bc754b0f
Sabhya <sabhya12.saini@gmail.com>
2022-01-11 18:52:04 +0530

    Update package.json

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
 const express = require("express");
-// const rateLimit = require("express-rate-limit");
-
 const connectDB = require("./config/db");
 const path = require("path");
 const app = express();
-
-// Create the rate limit rule
-// const apiRequestLimiter = rateLimit({
-//   windowMs: 1 * 60 * 1000, // 1 minute
-//   max: 2, // limit each IP to 2 requests per windowMs
-// });
-
-// Use the limit rule as an application middleware
-// app.use(apiRequestLimiter);
 
 // Connect Database
 connectDB();
