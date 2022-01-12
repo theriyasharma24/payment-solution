761bc56c959876c543ffa27cf73d3d60f094f1fa
Meetali <meetalimadhusmita69@gmail.com>
2022-01-12 15:28:08 +0530

    Meetali's Code Update

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React, { Fragment } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Alerts from "./components/layout/Alerts";
 import AuthState from "./context/auth/AuthState";
 import AlertState from "./context/alert/AlertState";
-import PrivateRoute from "./components/routing/PrivateRoute";
+// import PrivateRoute from "./components/routing/PrivateRoute";
 import Home from "./components/pages/Home";
 
 import "./App.css";
@@ -31,11 +31,11 @@ const App = () => {
             <Navbar />
             <Container>
               <Alerts />
-              <Switch>
-                <PrivateRoute exact path="/" component={Home} />{" "}
+              <Routes>
+                <Route exact path="/" component={Home} />{" "}
                 <Route exact path="/register" component={Register} />
                 <Route exact path="/login" component={Login} />
-              </Switch>
+              </Routes>
             </Container>
           </Fragment>
         </Router>
