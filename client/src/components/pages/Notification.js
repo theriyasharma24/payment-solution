7abbb28d02299938501dfc8f32786ddcaad318d0
Sulekha Bansal <geniusstar133@gmail.com>
2022-02-10 19:33:23 +0530

    sulekha

--- client/src/components/pages/Notification.js
+++ client/src/components/pages/Notification.js
@@ -1,38 +1,66 @@
 import React, { useContext } from "react";
-import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 
 import AuthContext from "../../context/auth/authContext";
+import { textAlign } from "@mui/system";
+import styled from "styled-components";
+import Button from "@mui/material/Button";
+
+const SubmitButton = styled(Button)`
+  && {
+    margin: 0;
+    margin-top: 0.5rem;
+    margin-inline: auto;
+    align-self: center;
+  }
+`;
 
 const Notification = () => {
   const authContext = useContext(AuthContext);
   const { user } = authContext;
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-    <Grid container spacing={2}>
-      <Grid item xs={6} md={8} 
-            style={{backgroundColor:"blue"}}
-            >
-        <p>xs=6 md=8</p>
-      </Grid>
-      <Grid item xs={6} md={4}
-            style={{backgroundColor:"red"}}
-            >
-        <p>xs=6 md=4</p>
-      </Grid>
-      </Grid>
 
-      <Grid container spacing={2}>
-      <Grid item xs={6} md={4} 
-            style={{backgroundColor:"blue"}}
+
+
+
+    <Box sx={{ flexGrow: 1 }}>
+    <Grid container spacing={1}>
+      <Grid item xs={6} md={6} 
+            style={{backgroundColor:"#95D554"} }
             >
-        <p>xs=6 md=8</p>
+        <SubmitButton onclick="myfunction()"style={{backgroundColor:"#6CA382"} } >NOTIFICATION</SubmitButton>
+      </Grid>
+      <Grid item xs={6} md={6}
+            style={{backgroundColor:"#95D554"}}
+            >
+        <SubmitButton onclick="myfunction()"style={{backgroundColor:"#FAD0D0"}}>REMINDER</SubmitButton>
+      </Grid>
+      </Grid> 
+
+      <Grid container>
+      <Grid item xs={6} md={4} 
+            style={{backgroundColor:"#FAD0D0"}}
+            >
+       <Box>
+       <div style={{backgroundColor:"#D72631"}}>
+          <p>EVENT:</p>
+          <p>Event Name:</p>
+          <p>Time of Event:</p>
+          <p>Time to Notify:</p>
+          <p>Date of Event:</p>
+          <p>Venue of Event:</p>
+        </div>
+        <div>
+        <SubmitButton onclick="myfunction()"style={{backgroundColor:"#FAD0D0"}}>CANCEL</SubmitButton>
+        <SubmitButton onclick="myfunction()"style={{backgroundColor:"#FAD0D0"}}>SAVE</SubmitButton>
+        </div>
+       </Box>
       </Grid>
       <Grid item xs={6} md={8}
-            style={{backgroundColor:"red"}}
+            style={{backgroundColor:"green"}}
             >
         <p>xs=6 md=4</p>
       </Grid>
